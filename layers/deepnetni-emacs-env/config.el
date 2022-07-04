175ee19c112c9cf5489ff81947c1c31c522de6b5
niye <deepnetni@163.com>
2022-07-04 14:14:39 +0800

    remove the question of whether to keep the tag when startup

--- layers/deepnetni-emacs-env/config.el
+++ layers/deepnetni-emacs-env/config.el
@@ -14,7 +14,9 @@
 ;(add-hook 'after-change-major-mode-hook 'purge-minor-modes)
 ;(add-hook 'after-init-hook 'spacemacs/toggle-centered-point-on)
 
-;; #################### lsp ###################
+;; ##############################################################
+;; #################### lsp ####################
+;; ##############################################################
 ;; use flycheck-verify-setup command to choose code check backends
 
 ;(with-eval-after-load 'c-c++-mode (add-hook 'c++-mode-hook 'lsp))
@@ -28,7 +30,9 @@
 ;                               (set (make-local-variable 'company-backends)
 ;                                    '(company-anaconda company-dabbrev company-ispell))))
 
+;; ##############################################################
 ;; #################### self minor mode part ####################
+;; ##############################################################
 (setq deepnetni-emacs-env--goto-center-hook
       #'(evil-goto-mark
          ;evil-ex-search-next
@@ -93,3 +97,4 @@
 (eval-after-load 'dired-quick-sort '(setq dired-quick-sort-suppress-setup-warning t))
 (setq python-indent-guess-indent-offset t)
 (setq python-indent-guess-indent-offset-verbose nil)
+(setq tags-add-tables nil)
